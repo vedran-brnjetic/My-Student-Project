c485a44c8749a78ae0bcad2121ee8aabd11fdd6b
vedran-brnjetic <Vedran.Brnjetic@metropolia.fi>
2017-08-30 09:57:07 +0300

    Restructured the files to prepare for CMS implemetation

---
+++ structure/js.php
@@ -0,0 +1,18 @@
+<script type="text/javascript" src="assets/plugins/jquery-2.2.3.min.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery.cookie.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery.appear.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery.isotope.js"></script>
+<script type="text/javascript" src="assets/plugins/masonry.js"></script>
+
+<script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/plugins/magnific-popup/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="assets/plugins/owl-carousel/owl.carousel.min.js"></script>
+<script type="text/javascript" src="assets/plugins/stellar/jquery.stellar.min.js"></script>
+<script type="text/javascript" src="assets/plugins/knob/js/jquery.knob.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery.backstretch.min.js"></script>
+<script type="text/javascript" src="assets/plugins/superslides/dist/jquery.superslides.min.js"></script>
+<script type="text/javascript" src="assets/plugins/styleswitcher/styleswitcher.js"></script><!-- STYLESWITCHER - REMOVE ON PRODUCTION/DEVELOPMENT -->
+
+
+<script type="text/javascript" src="assets/js/scripts.js"></script>
---
+++ structure/page_footer.php
@@ -0,0 +1,85 @@
+<footer>
+
+  <!-- copyright , scrollTo Top -->
+  <div class="footer-bar">
+    <div class="container">
+      <span class="copyright">Copyright &copy; Your Company, LLC . All Rights Reserved.</span>
+      <a class="toTop" href="#topNav">BACK TO TOP <i class="fa fa-arrow-circle-up"></i></a>
+    </div>
+  </div>
+  <!-- copyright , scrollTo Top -->
+
+
+  <!-- footer content -->
+  <div class="footer-content">
+    <div class="container">
+
+      <div class="row">
+
+
+        <!-- FOOTER CONTACT INFO -->
+        <div class="column col-md-4">
+          <h3>CONTACT</h3>
+
+          <p class="contact-desc">
+            You can have a website like this. Just ask me.
+          </p>
+          <address class="font-opensans">
+            <ul>
+              <li class="footer-sprite address">
+                Kirstinharju 3c 37<br />
+                Espoo<br />
+                Finland<br />
+              </li>
+              <li class="footer-sprite phone">
+                <a href="tel:+358413639501">Phone: +358 (0)41 363 9501</a>
+              </li>
+              <li class="footer-sprite email">
+                <a href="mailto:Vedran.Brnjetic@metropolia.fi">Vedran.Brnjetic@metropolia.fi</a>
+              </li>
+            </ul>
+          </address>
+
+        </div>
+        <!-- /FOOTER CONTACT INFO -->
+
+
+        <!-- FOOTER LOGO -->
+        <div class="column logo col-md-4 text-center">
+          <div class="logo-content">
+            <!--<img class="animate_fade_in" src="assets/images/logo_footer.png" width="200" alt="" />-->
+            <h4>Cro Viking</h4>
+          </div>
+        </div>
+        <!-- /FOOTER LOGO -->
+
+
+        <!-- FOOTER LATEST POSTS --><!--
+        <div class="column col-md-4 text-right">
+          <h3>RECENT POSTS</h3>
+
+          <div class="post-item">
+            <small>JANUARY 2, 2014 BY ADMIN</small>
+            <h3><a href="blog-single-sidebar-left.html">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
+          </div>
+          <div class="post-item">
+            <small>JANUARY 2, 2014 BY ADMIN</small>
+            <h3><a href="blog-single-sidebar-left.html">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
+          </div>
+          <div class="post-item">
+            <small>JANUARY 2, 2014 BY ADMIN</small>
+            <h3><a href="blog-single-sidebar-left.html">Lorem ipsum dolor sit amet, consectetur adipiscing elit</a></h3>
+          </div>
+
+          <a href="blog-masonry-sidebar.html" class="view-more pull-right">View Blog <i class="fa fa-arrow-right"></i></a>
+
+        </div>-->
+        <!-- /FOOTER LATEST POSTS -->
+
+      </div>
+
+    </div>
+  </div>
+  <!-- footer content -->
+
+</footer>
