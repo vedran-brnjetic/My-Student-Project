799e536d17ab761f802f2a624aa544900eccacf2
vedran-brnjetic <Vedran.Brnjetic@metropolia.com>
2017-08-30 08:10:24 +0300

    Starting the git approach to this website

---
+++ structure/top-bar.php
@@ -0,0 +1,51 @@
+<!-- Top Bar -->
+<header id="topHead">
+  <div class="container">
+
+    <!-- PHONE/EMAIL -->
+    <span class="quick-contact pull-left">
+      <a href="tel:+358413639501"><i class="fa fa-phone"></i> +358 41 363 9501  &bull;</a>
+      <a class="" href="mailto:Vedran.Brnjetic@metropolia.fi">Vedran.Brnjetic@metropolia.fi  &bull;</a>
+      <a href="#"><i class="fa fa-microchip"></i> Vedran Brnjetic (croViking)</a>
+    </span>
+    <!-- /PHONE/EMAIL -->
+
+    <!-- LANGUAGE -->
+    <!--
+    <div class="btn-group pull-right hidden-xs">
+      <button class="dropdown-toggle language" type="button" data-toggle="dropdown">
+        <img src="assets/images/flags/us.png" width="16" height="11" alt="EN Language" /> English <span class="caret"></span>
+      </button>
+
+      <ul class="dropdown-menu">
+        <li>
+          <a href="#">
+            <img src="assets/images/flags/us.png" width="16" height="11" alt="EN Language" /> [US] English
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="assets/images/flags/de.png" width="16" height="11" alt="DE Language" /> [DE] German
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="assets/images/flags/fr.png" width="16" height="11" alt="FR Language" /> [FR] French
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <img src="assets/images/flags/ru.png" width="16" height="11" alt="RU Language" /> [RU] Russian
+          </a>
+        </li>
+      </ul>
+    </div>-->
+    <!-- /LANGUAGE -->
+
+
+
+
+
+  </div>
+</header>
+<!-- /Top Bar -->
---
+++ structure/top-nav.php
@@ -0,0 +1,41 @@
+<!-- TOP NAV -->
+<header id="topNav" class="topHead"><!-- remove class="topHead" if no topHead used! -->
+  <div class="container">
+
+    <!-- Mobile Menu Button -->
+    <button class="btn btn-mobile" data-toggle="collapse" data-target=".nav-main-collapse">
+      <i class="fa fa-bars"></i>
+    </button>
+
+    <!-- Logo text or image -->
+
+    <!-- Top Nav -->
+    <div class="navbar-collapse nav-main-collapse collapse pull-right">
+      <nav class="nav-main mega-menu">
+        <ul class="nav nav-pills nav-main scroll-menu" id="topMain">
+          <li class="dropdown">
+            <a class="dropdown-toggle" href="#">
+              Home <i class="fa fa-angle-down"></i>
+            </a>
+          </li>
+          <li class="dropdown mega-menu-item mega-menu-two-columns">
+            <a class="dropdown-toggle" href="#">
+              Pages <i class="fa fa-angle-down"></i>
+            </a>
+          </li>
+          <li class="dropdown mega-menu-item mega-menu-fullwidth">
+            <a class="dropdown-toggle" href="#">
+              Features <i class="fa fa-angle-down"></i>
+            </a>
+          </li>
+
+
+
+
+        </ul>
+      </nav>
+    </div>
+    <!-- /Top Nav -->
+
+  </div>
+</header>
