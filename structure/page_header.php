800f8de2367fe17f08700c978cd539a996ea496c
vedran-brnjetic <Vedran.Brnjetic@metropolia.fi>
2017-08-30 10:38:14 +0300

    Page header correction

---
+++ structure/page_header.php
@@ -0,0 +1,16 @@
+<header id="page-title"> <!-- style="background-image:url('assets/images/demo/parallax_bg.jpg')" -->
+  <!--
+    Enable only if bright background image used
+    <span class="overlay"></span>
+  -->
+
+  <div class="container">
+    <h1>Portfolio!</h1>
+
+    <ul class="breadcrumb">
+      <li><a href="index.html">Home</a></li>
+      <li><a href="#">Portfolio</a></li>
+      <li class="active">Gallery</li>
+    </ul>
+  </div>
+</header>
