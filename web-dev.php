3ac02c1ad813bca493b7d74b62327016f980e6c5
Vedran Brnjetic <vedranb@metropolia.fi>
2017-10-02 18:32:32 +0300

    css assignment

--- web-dev.php
+++ web-dev.php
@@ -15,15 +15,15 @@
 
 								<!-- article title -->
 								<div class="item-title">
-									<h2><a href="blog-single-sidebar-left.html">Lecture notes</a></h2>
-									<a href="#" class="label label-default light"><i class="fa fa-dot-circle-o"></i> Business</a> 
+									<h2><a href="#">CSS Lecture notes</a></h2>
+									<a href="#" class="label label-default light"><i class="fa fa-dot-circle-o"></i> Lectures</a> 
 									<a href="#" class="scrollTo label label-default light"><i class="fa fa-comment-o"></i> 3 Comments</a>
-									<span class="label label-default light">June 29, 2014</span> 
+									<span class="label label-default light">September 29, 2017</span> 
 								</div>
 
 								<!-- image -->
 								<figure>
-									<img src="http://www.codingdojo.com/blog/wp-content/uploads/css.jpg" class="img-responsive" alt="img" />
+									<img src="assets/images/html_work/css3.png" class="img-responsive" alt="css 3 logo" />
 								</figure>
 
 								<!-- blog short preview -->
@@ -57,45 +57,39 @@
 						</article>
 						<article class="timeline-item pull-right">
 							<div class="item-date">
-								29 <span>Sep</span>
+								22 <span>Sep</span>
 							</div>
 
 							<div class="item">
 
 								<!-- article title -->
 								<div class="item-title">
-									<h2><a href="blog-single-sidebar-left.html">Lecture notes</a></h2>
-									<a href="blog.html" class="label label-default light"><i class="fa fa-dot-circle-o"></i> Business</a> 
-									<a href="blog-single-sidebar-left.html#comments" class="scrollTo label label-default light"><i class="fa fa-comment-o"></i> 3 Comments</a>
-									<span class="label label-default light">June 29, 2014</span> 
+									<h2><a href="#">HTML Lecture notes</a></h2>
+									<a href="blog.html" class="label label-default light"><i class="fa fa-dot-circle-o"></i> Lectures</a> 
+									<a href="blog-single-sidebar-left.html#comments" class="scrollTo label label-default light"><i class="fa fa-comment-o"></i> 1.2k Comments</a>
+									<span class="label label-default light">September 22, 2017</span> 
 								</div>
 
 								<!-- image -->
 								<figure>
-									<img src="http://www.codingdojo.com/blog/wp-content/uploads/css.jpg" class="img-responsive" alt="img" />
+									<img src="assets/images/html_work/html5.png" class="img-responsive" alt="img" />
 								</figure>
 
 								<!-- blog short preview -->
 								<p>
-									We talked a lot about css rules.<br/>Topics we covered:
+									Today was all about HTML basics<br/>Topics we covered:
 								</p>
 								<ul>
-									<li><strong>.css standard:</strong> history and versions</li>
-									<li><strong>Typography:</strong>font families, sizes, weight</li>
-									<li><strong>Relative vs. absolute size:</strong> em, px, %</li>
-									<li><strong>Ul lists:</strong> ul, li, list styles</li>
-									<li><strong>Colours:</strong> Diferences between colour notations - rgb, rgba, hex, HSL<br/>
-									radial gradient example (need to use <em>background-image</em> property)<br>
-									linear gradient example									
-									</li>
-									<li><strong>Body background:</strong> Whatever you do, if you change the background color, change the foreground as well</li>
-									<li><strong>Background image:</strong> First thing, kill the default repetition.(mention of <em>background-repeat</em> options)<br>
-									Very extensive description of background image<br>
-									Multiple images in same <em>background-image</em> property									
-									</li>									
-									<li><strong>Exercise:</strong> Download two images and place into your home directory</li>
-									<li><strong>Borders:</strong> Shorthand notation, border radius, border image, box-shadow</li>
-									<li><strong>Transformations and Transitions:</strong> We love math, but we only have 10 minutes</li>
+									<li><strong>HTML:</strong> history and standards</li>
+									<li><strong>Structure:</strong> &lt;!Doctype html&gt; browser directive</li>									
+									<li><strong>Structure:</strong> &lt;html&gt; tag, &lt;/html&gt;</li>
+									<li><strong>Structure:</strong> &lt;head&gt; tag, &lt;body&gt; tag</li>
+									<li><strong>Structure:</strong> Tags attributes and values</li>									
+									<li><strong>Escape artist:</strong><em> &amp; </em> symbol and significance in the markup</li>
+									<li><strong>Lists:</strong> ul, li, list styles</li>
+									<li><strong>Connecting stuff:</strong> links and anchors - the difference</li>
+									<li><strong>Text:</strong> Headings, paragraphs (p), labels</li>
+									<li><strong>Images:</strong> Figures, img, captions, alt attributes and src attribute</li>									
 								</ul>
 
 								<!-- read more button -->
